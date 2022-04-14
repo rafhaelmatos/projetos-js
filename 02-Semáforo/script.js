f270f66c06d6eba09a257a8d679bf19d8bed4036
Rafhael Matos <rafhaelcg19@gmail.com>
2022-04-14 16:52:11 +0000

    14

--- 02-Semáforo/script.js
+++ 02-Semáforo/script.js
@@ -1,1 +1,19 @@
-const img = document.getElementById(`img`)
+const img = document.getElementById('img')
+const buttons = document.getElementById('buttons')
+
+const trafficlight = (event) => {
+    turnOn[event.target.id]()
+}
+
+const changeColor = () => {
+    const color = 'red';
+    turnOn[color]()
+}
+const turnOn = {
+    'red': () => img.src = 'img/vermelho.png',
+    'yellow': () => img.src = 'img/amarelo.png',
+    'green': () => img.src = 'img/verde.png',
+    'automatic': () => setInterval(changeColor, 1000)
+}
+
+buttons.addEventListener(`click`, trafficlight)
